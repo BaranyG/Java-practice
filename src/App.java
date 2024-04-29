20537ce4b560a8de90f5d0a47b521d1fee7d7e22
BaranyG <72973458+BaranyG@users.noreply.github.com>
2024-04-29 20:23:23 +0200

    Frame

--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-import javax.swing.JFrame;
-
 public class App {
     public App(){
 
@@ -13,13 +11,6 @@ public class App {
         System.out.println(joska.name);
         System.out.println(joska.hand[0] + joska.hand[1] );
 
-
-
-        
-        JFrame frame = new JFrame();
-        frame.setSize(640,480);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Test Frame");
-        frame.setVisible(true);
+        new Frame();
     }
 }
