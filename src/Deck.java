8948c5d43fb645db271ad1c3215647f1ca2c5096
BaranyG <72973458+BaranyG@users.noreply.github.com>
2024-05-04 14:36:55 +0200

    rename variables to english

--- src/Deck.java
+++ src/Deck.java
@@ -2,16 +2,16 @@ import java.util.Random;
 
 public class Deck {
     //String array[][];
-    String[] pakli = new String[52];
+    String[] deck = new String[52];
 
     public Deck(){
-        String[][] kartyak = new String[][]{ {"Pikk", "Karo", "Treff", "Kor"}, {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"} };
+        String[][] cards = new String[][]{ {"Pikk", "Karo", "Treff", "Kor"}, {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"} };
 //                                                    [0]                                         [1]
 //                                                    [3]                                         [12]              
-        // Convert from 2D kartyak array to 1D pakli array
-        for (int i = 0; i < kartyak[0].length; i++){
-            for (int j = 0; j < kartyak[1].length; j++) {
-                pakli[((j+1)+(i*13))-1] = kartyak[0][i] + " " + kartyak[1][j];
+        // Convert from 2D cards array to 1D deck array
+        for (int i = 0; i < cards[0].length; i++){
+            for (int j = 0; j < cards[1].length; j++) {
+                deck[((j+1)+(i*13))-1] = cards[0][i] + " " + cards[1][j];
             }
         }
     }
--- src/Player.java
+++ src/Player.java
@@ -10,35 +10,35 @@ public class Player {
     public Player(String name){
         this.name = name;
 
-        String[] pakli = new Deck().pakli;
+        String[] deck = new Deck().deck;
         
-        // Shuffle the pakli2 array
-        pakli = Deck.shuffle(pakli);
+        // Shuffle the deck2 array
+        deck = Deck.shuffle(deck);
 
-        // Print the shuffled pakli2 deck
-        for (String card : pakli) {
+        // Print the shuffled deck2 deck
+        for (String card : deck) {
             System.out.println(card);
         }
         
-        this.hand[0] = pakli[0];
-        this.hand[1] = pakli[1];
+        this.hand[0] = deck[0];
+        this.hand[1] = deck[1];
     }
 
     public Player(){
         this.name = "Joska";
         
-        String[] pakli = new Deck().pakli;
+        String[] deck = new Deck().deck;
         
-        // Shuffle the pakli2 array
-        pakli = Deck.shuffle(pakli);
+        // Shuffle the deck2 array
+        deck = Deck.shuffle(deck);
 
-        // Print the shuffled pakli2 deck
-        for (String card : pakli) {
+        // Print the shuffled deck2 deck
+        for (String card : deck) {
             System.out.println(card);
         }
         
-        this.hand[0] = pakli[0];
-        this.hand[1] = pakli[1];
+        this.hand[0] = deck[0];
+        this.hand[1] = deck[1];
     }
 
 
