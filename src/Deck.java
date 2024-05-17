1cc99ec9e515867c7512b93011018dae675c8931
BaranyG <72973458+BaranyG@users.noreply.github.com>
2024-05-17 21:42:14 +0200

    Deck is now Singleton

--- src/Deck.java
+++ src/Deck.java
@@ -2,31 +2,44 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Deck {
-    //String[] deck = new String[52];
-    ArrayList<String> deck = new ArrayList<String>(52);
+    private static Deck instance;
+    
+    public ArrayList<String> deck = new ArrayList<String>(52);
 
 
-    public Deck(){
+    private Deck(){
         String[][] cards = new String[][]{ 
             {"Pikk", "Karo", "Treff", "Kor"}, //[0][3]
             {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"} }; //[1][12]
                     
-        // Convert from 2D cards array to ArrayList
+        // Convert from 2D Array cards to ArrayList deck
         for (String color : cards[0]){
             for (String card : cards[1]){
                 deck.add(color + " " + card) ;
             }
         }
     }
-    
 
+
+    public static Deck get(){
+        if(instance == null){
+            instance = new Deck();
+        }
+        return instance;
+    }
+    
+    /**
+     * 
+     * @param ArrayList<String>
+     * @return ArrayList
+     */
     public static ArrayList<String> shuffle(ArrayList<String> array){
         Random rand = new Random();
         
         for (int i = 0; i < array.size(); i++){
-            int rand_int = rand.nextInt(array.size());
-            String tmp = array.get(rand_int);    
-            array.set(rand_int, array.get(i));
+            int randomInt = rand.nextInt(array.size());
+            String tmp = array.get(randomInt);    
+            array.set(randomInt, array.get(i));
             array.set(i, tmp);
         }
         return array;
