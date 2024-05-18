40969876ee32cc20ef3dfed2b52a06cfe4e27941
BaranyG <72973458+BaranyG@users.noreply.github.com>
2024-05-18 08:04:27 +0200

    Use draw instead of ArrayList get, so the cards are removed after draw

--- src/Player.java
+++ src/Player.java
@@ -5,39 +5,24 @@ public class Player {
     String hand1;
     String hand2;
 
-    public Player(String name, String hand1, String hand2){
-        this.name = name;
-        this.hand1 = hand1;
-        this.hand2 = hand2;
-    }
-
-    public Player(String name){
-        this.name = name;
-
-        ArrayList<String> deck = Deck.get().deck;
-        
-        // Shuffle the deck array
-        deck = Deck.shuffle(deck);
-        
-        this.hand1 = deck.get(0);
-        this.hand2 = deck.get(1);
-    }
-
-    public Player(String hand1, String hand2){
-        this.name = "Joseph";
-        this.hand1 = hand1;
-        this.hand2 = hand2;
-    }
 
     public Player(){
         this.name = "Joseph";
         
         ArrayList<String> deck = Deck.get().deck;
-
-        // Shuffle the deck array
-        deck = Deck.shuffle(deck);
         
-        this.hand1 = deck.get(0);
-        this.hand2 = deck.get(1);
+        this.hand1 = Deck.draw(deck);
+        this.hand2 = Deck.draw(deck);
     }
+
+    
+    public Player(String name){
+        this.name = name;
+
+        ArrayList<String> deck = Deck.get().deck;
+        
+        this.hand1 = Deck.draw(deck);
+        this.hand2 = Deck.draw(deck);
+    }
+
 }
